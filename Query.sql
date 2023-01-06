6d06fafdac5b89ac906b2f84f794b5b306daf16e
fedorenko <fedorenko@mego.travel>
2023-01-06 14:18:50 +0300

    HHTask + Query

---
+++ Query.sql
@@ -0,0 +1,6 @@
+﻿SELECT
+	Product.product_name 'Product',
+	COALESCE(Category.category_name, 'Out of category') 'Category'
+FROM Product
+LEFT JOIN ProductCategory ON Product.product_id = ProductCategory.product_id
+LEFT JOIN Category ON Category.category_id = ProductCategory.category_id;
