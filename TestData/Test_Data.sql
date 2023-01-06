d77386c9263daace1433094c3f2e26386e299c32
fedorenko <fedorenko@mego.travel>
2023-01-06 14:18:36 +0300

    HHTask + preparing test data

---
+++ TestData/Test_Data.sql
@@ -0,0 +1,30 @@
+﻿USE Products;
+GO
+
+INSERT INTO Product(product_name)
+VALUES
+	('Water'),
+	('Apple'),
+	('Whiskey'),
+	('Chewing gum'),
+	('Cap'),
+	('Perfume'),
+	('Chupa-chups');
+
+INSERT INTO Category(category_name)
+VALUES
+	('Drinks'),
+	('Fruit'),
+	('Alcohol'),
+	('Candies'),
+	('For home'),
+	('Clothes'),
+	('Perfumery');
+	
+INSERT INTO ProductCategory(product_id, category_id)
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(5, 6),
+	(6, 7);
